f2df5337f048a71fa0c64e8763dcc97cff6f21d8
Kazumi Inada <hello@nandenjin.com>
2022-04-21 16:05:04 +0900

    chore: Init with create-vite-app and vite-plugin-chrome-extension

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { chromeExtension } from "vite-plugin-chrome-extension";
+
+// https://vitejs.dev/config/
+export default defineConfig({build: {
+  rollupOptions: {
+      input: "src/manifest.json"
+  }
+},
+plugins: [
+  chromeExtension(),
+  vue()
+]
+})
